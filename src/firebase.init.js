d8567a75cd96303da2820101bd80567332e2df48
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-07 00:50:47 +0600

    resolve react 18 error and show google signin popup

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHxRU_eURooKsJ0T8tB54MsVvpCApAzwg",
+  authDomain: "simple-firebase-authenic-dba51.firebaseapp.com",
+  projectId: "simple-firebase-authenic-dba51",
+  storageBucket: "simple-firebase-authenic-dba51.appspot.com",
+  messagingSenderId: "25305739352",
+  appId: "1:25305739352:web:d69f0791f6546175381a2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
