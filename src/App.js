18ba19d4ba5698284a9ba12eb0daba005329f90c
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-07 12:24:06 +0600

    signin pop up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import app from './firebase.init';
 import { getAuth, signInWithPopup } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
   const googleSignInHandler = () => {
     
@@ -12,16 +14,22 @@ function App() {
     signInWithPopup(auth, provider)
     .then(result =>{
       const user = result.user;
-      console.log(user)
+      setUser(user)
     })
     .catch(error => {
       console.log(error)
     })
 
   }
+  // console.log(user)
+  const {email, displayName, photoURL} = user
 
   return (
     <div className="App">
+      <p>Name : {displayName}</p>
+      <p>Email : {email}</p>
+      <img src={photoURL} alt="" />
+      <br />
       <button onClick={googleSignInHandler}>Sign in with Google</button>
 
     </div>
